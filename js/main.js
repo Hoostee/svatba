c030470c52e38e81d87ba92b85f854f8b783803b
Václav Jirák <vaclav.jirak@unicorn.com>
2023-03-21 11:08:05 +0100

    v0.000001

--- js/main.js
+++ js/main.js
@@ -1,117 +1,111 @@
 (function ($) {
     "use strict";
 
-    // Navbar on scrolling
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-            $('.navbar').fadeIn('slow').css('display', 'flex');
-        } else {
-            $('.navbar').fadeOut('slow').css('display', 'none');
+    const evaluatingDelayMs = 10;
+
+    let correctAnswers = 0;
+    let currentQuestionIndex = 0;
+
+    $(questionsContainer).show();
+    $(resultContainer).hide();
+    $(weddingInfoContainer).hide();
+
+    let questionList = [
+        {
+            questionText: "Kolik v současnosti zbývá Markétce zubů?",
+            options: ["7", "27", "32", "47"],
+            correctAnswer: "27"
+        },
+        {
+            questionText: "Který z následujících psisek je největší basťoch?",
+            options: ["Kouďáček", "Fíbí", "Skřetisko", "Ginuška"],
+            correctAnswer: "Fíbí"
         }
-    });
+    ];
 
+    
+    showQuestion(questionList[currentQuestionIndex]);
 
-    // Smooth scrolling on the navbar links
-    $(".navbar-nav a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            
-            $('html, body').animate({
-                scrollTop: $(this.hash).offset().top - 45
-            }, 1500, 'easeInOutExpo');
-            
-            if ($(this).parents('.navbar-nav').length) {
-                $('.navbar-nav .active').removeClass('active');
-                $(this).closest('a').addClass('active');
+    $(enterButton).click(showWeddingInfo);
+
+    function showQuestion(question) {
+        $("body").css("background-color", "white");
+        $(questionLabel).css("color", "orange");
+        $(questionAnswers).empty();
+
+        $(questionLabel).text(question.questionText);
+
+        for (const option of question.options) {
+            let button = $("<button></button>")
+                .text(option)
+                .click(function () { handleAnswer(question, option); });
+            $(questionAnswers).append(button);
+        }
+    }
+
+    function handleAnswer(question, answer) {
+
+        $(questionAnswers).empty();
+
+        setEvaluating().then(() => {
+            if (question.correctAnswer === answer) {
+                handleCorrectAnswer()
+            } else {
+                handleIncorrectAnswer(question);
             }
-        }
-    });
 
-
-    // Modal Video
-    $(document).ready(function () {
-        var $videoSrc;
-        $('.btn-play').click(function () {
-            $videoSrc = $(this).data("src");
+            if (++currentQuestionIndex < questionList.length) {
+                $(questionAnswers).append($("<button></button>")
+                    .text("Další otázka")
+                    .click(() => { showQuestion(questionList[currentQuestionIndex]); }));
+            } else {
+                $(questionAnswers).append($("<button></button>")
+                    .text("Pokračovat")
+                    .click(allQuestionsAnswered));
+            }
+            
         });
-        console.log($videoSrc);
 
-        $('#videoModal').on('shown.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-        })
+    }
 
-        $('#videoModal').on('hide.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc);
-        })
-    });
+    async function setEvaluating() {
+        $(questionLabel).text("Vypočítávám...");
+        return new Promise(r => setTimeout(r, evaluatingDelayMs));
+    }
 
-
-    // Scroll to Bottom
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.scroll-to-bottom').fadeOut('slow');
-        } else {
-            $('.scroll-to-bottom').fadeIn('slow');
-        }
-    });
-
-
-    // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
-        $(this).addClass('active');
-
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
-    });
+    function handleCorrectAnswer() {
+        $(questionLabel).css("color", "white");
+        $("body").css("background-color", "green");
+        $(questionLabel).text("Výborně!");
+        correctAnswers++;
+    }
     
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+    function handleIncorrectAnswer(question) {
+        $(questionLabel).css("color", "white");
+        $("body").css("background-color", "red");
+        $(questionLabel).text("Špatně!!! Správná odpověď: " + question.correctAnswer);
+    }
 
+    function allQuestionsAnswered() {
+        $(questionsContainer).hide();
+        $(resultContainer).show();
 
-    // Gallery carousel
-    $(".gallery-carousel").owlCarousel({
-        autoplay: false,
-        smartSpeed: 1500,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:2
-            },
-            768:{
-                items:3
-            },
-            992:{
-                items:4
-            },
-            1200:{
-                items:5
-            }
-        }
-    });
-    
+        $(resultLabel).text("Počet správných odpovědí: " + correctAnswers + " / " + questionList.length);
+        $(assessmentLabel).text(resolveAssessmentLabel());
+        $("body").css("background-color", "white");
+        console.log(correctAnswers + "/" + questionList.length);
+    }
+
+    function resolveAssessmentLabel() {
+        // TODO
+        return "Hustý! Zveme Tě na";
+    }
+
+    function showWeddingInfo() {
+        $(questionsContainer).hide();
+        $(resultContainer).hide();
+        $(weddingInfoContainer).show();
+    }
+
 })(jQuery);
 
