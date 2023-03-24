68ba4f2ed044d12697f871f0e2f60a8a8fc94a6e
Václav Jirák <vaclav.jirak@unicorn.com>
2023-03-24 19:27:19 +0100

    nevim

--- js/main.js
+++ js/main.js
@@ -6,26 +6,51 @@
     let correctAnswers = 0;
     let currentQuestionIndex = 0;
 
-    $(questionsContainer).show();
-    $(resultContainer).hide();
-    $(weddingInfoContainer).hide();
-
     let questionList = [
         {
-            questionText: "Kolik v současnosti zbývá Markétce zubů?",
-            options: ["7", "27", "32", "47"],
-            correctAnswer: "27"
+            questionText: "Kolik zbývá v současnosti Markétce zubů?",
+            options: ["5", "25", "32", "45"],
+            correctAnswer: "25"
         },
         {
-            questionText: "Který z následujících psisek je největší basťoch?",
+            questionText: "Které z následujících psisek je největší basťoch?",
             options: ["Kouďáček", "Fíbí", "Skřetisko", "Ginuška"],
             correctAnswer: "Fíbí"
+        },
+        {
+            questionText: "Po kom se Václavíček jmenuje?",
+            options: ["Táta", "Děda", "Máma", "Strejda"],
+            correctAnswer: "Máma"
+        },
+        {
+            questionText: "Jaké zvíře kouslo Makrelíčka do nohy v zoo?",
+            options: ["Želva", "Fíbí", "Velbloud", "Papouch"],
+            correctAnswer: "Velbloud"
+        },
+        {
+            questionText: "Kde Václavíček požádal Makrelíčka o ruku?",
+            options: ["Sokolov", "Geirangerfjord", "Geyrangerfjord", "Korsika"],
+            correctAnswer: "Geirangerfjord"
         }
     ];
 
+    let correctAnswersToAssesment = [
+        "Jsi blbeček a vůbec nás neznáš, ale přesto Tě zveme na",
+        "Moc nás teda neznáš, ale přesto Tě zveme na",
+        "Nic moc, je tu prostor pro zlepšení, ale přesto Tě zveme na",
+        "To ujde! A tak Tě zveme na",
+        "Jsi náš kámoš, tak Ti jednu chybičku odpustíme a zveme Tě na",
+        "Hustý, jsi borec! A proto Tě zveme na"
+    ]
+
+    $(entryPointContainer).show();
+    $(questionsContainer).hide();
+    $(resultContainer).hide();
+    $(weddingInfoContainer).hide();
     
     showQuestion(questionList[currentQuestionIndex]);
 
+    $(enterQuestionerButton).click(showQuestions);
     $(enterButton).click(showWeddingInfo);
 
     function showQuestion(question) {
@@ -90,18 +115,21 @@
         $(questionsContainer).hide();
         $(resultContainer).show();
 
-        $(resultLabel).text("Počet správných odpovědí: " + correctAnswers + " / " + questionList.length);
-        $(assessmentLabel).text(resolveAssessmentLabel());
+        $(resultLabel).text(correctAnswers + " / " + questionList.length);
+        $(assessmentLabel).text(correctAnswersToAssesment[correctAnswers]);
         $("body").css("background-color", "white");
         console.log(correctAnswers + "/" + questionList.length);
     }
 
-    function resolveAssessmentLabel() {
-        // TODO
-        return "Hustý! Zveme Tě na";
+    function showQuestions() {
+        $(entryPointContainer).hide();
+        $(questionsContainer).show();
+        $(resultContainer).hide();
+        $(weddingInfoContainer).hide();
     }
 
     function showWeddingInfo() {
+        $(entryPointContainer).hide();
         $(questionsContainer).hide();
         $(resultContainer).hide();
         $(weddingInfoContainer).show();
